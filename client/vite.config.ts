90354ac3b736ea68ccd00a42a5032b020fc2e756
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-06-30 20:58:40 +0530

    feat: add Docker configuration and FastAPI server setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true, // or "0.0.0.0" // allow access from Docker container
+    port: 5173,
+    allowedHosts: true,
+    watch: {
+      usePolling: true, // enables hot reload inside Docker
+    },
+  },
 });
